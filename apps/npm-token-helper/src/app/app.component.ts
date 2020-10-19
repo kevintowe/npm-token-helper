e437848c9d6441df2aa5770ac6010655b5d0a4ab
Kevin Towe <kevintowe@Kevins-iMac.attlocal.net>
2020-10-19 18:53:13 -0500

    initial commit

---
+++ apps/npm-token-helper/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'npm-token-helper-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'npm-token-helper';
+}
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/npm-token-helper'],
+};
