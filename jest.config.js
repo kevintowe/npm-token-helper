61689d5e23eeb7c673a282a9aaf78243b5092bed
Kevin Towe <kevintowe@Kevins-iMac.attlocal.net>
2020-10-19 19:16:20 -0500

    init

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  projects: ['<rootDir>/apps/npm-token-helper'],
+  projects: [
+    '<rootDir>/apps/npm-token-helper',
+    '<rootDir>/libs/pages',
+    '<rootDir>/libs/components',
+    '<rootDir>/libs/services',
+  ],
 };
